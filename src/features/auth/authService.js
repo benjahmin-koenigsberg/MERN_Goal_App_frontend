50a55131448e765d4c1fd5647589e2531793e51a
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-12-18 18:32:02 -0500

    Update authService.js
    add backslash

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://mern-goal-app-backend.onrender.com/api/users';
+const API_URL = 'https://mern-goal-app-backend.onrender.com/api/users/';
 
 
 //Register user
